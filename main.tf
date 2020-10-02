251ee4ac4f0c424f387484d2a71300a25ad57bb3
Loris Strozzini <lstrozzini@gmail.com>
2020-10-02 16:26:56 +0200

    Changed AZ

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "jenkins" {
 	ami = "ami-0947d2ba12ee1ff75"
 	instance_type = "t2.micro"
     key_name = "terraform"
-    availability_zone = "us-east-1-a"
+    availability_zone = "us-east-1c"
 	user_data = file("install_jenkins.sh")
 	tags = {
 		Name = "Jenkins"	
